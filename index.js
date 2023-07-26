67fd7c973374df391a30e7929deb3365b37c0a87
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2023-07-26 23:42:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const store = require('./app/store')
+const cakeActions =require('./features/cake/cakeSlice').cakeActions
+const icecreamActions =require('./features/iceCream/iceCreamSlice').icecreamAction
+
+console.log("initialState", store.getState())
+
+const unsubscribe = store.subscribe(()=>{
+    console.log("updatedState", store.getState())   //Redux-logger will take care.
+})
+
+store.dispatch(cakeActions.ordered(3))
+store.dispatch(cakeActions.restocked(5))
+
+store.dispatch(icecreamActions.ordered(7))
+store.dispatch(icecreamActions.restocked(15))
+
+
+unsubscribe()
